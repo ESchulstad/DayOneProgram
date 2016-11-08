6f0d648b04f2f7ca98dd19f5a9b7f4b548ca0ef2
ESchulstad <eschulstad@gmail.com>
2016-11-07 20:55:28 -0500

    initial commit

---
+++ DayOneProgram/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayOneProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Hello world!");
+            //Console.WriteLine("Hello " +name+ ".");
+
+            Console.WriteLine("Enter two numbers to be added together.");
+
+            int numberOne = int.Parse(Console.ReadLine());
+            
+            int numberTwo = int.Parse( Console.ReadLine());
+           
+
+            int finalNumber = numberOne + numberTwo;
+
+            Console.WriteLine(finalNumber);
+        }
+    }
+}
